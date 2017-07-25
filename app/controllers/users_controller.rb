9385768e346d83ef57ac493bc90f718f3093112e
R-growth01 <fengkaicloud2@fengkaideMacBook-Air.local>
2017-07-25 15:00:02 +0800

    edit user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,19 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_user
+
+  def show
+  end
 
   def edit
-    @user = current_user
+     @user = current_user
+     @user.create_profile unless @user.profile
   end
 
   def update
-    @user = current_user
 
     if @user.update(user_params)
-      redirect_to edit_user_path
+      redirect_to user_path
       flash[:notice] = "修改成功"
     else
       render "edit"
@@ -18,7 +22,12 @@ class UsersController < ApplicationController
 
   protected
 
+  def find_user
+    @user = current_user
+    @user.create_profile unless @user.profile
+  end
+
   def user_params
-    params.require(:user).permit(:time_zone)
+    params.require(:user).permit(:time_zone, :profile_attributes => [:id, :legal_name, :birthday, :location, :education, :occupation, :bio, :specialty] )
   end
 end
